f1eda01513b4019cd2333a9065f2f4981193fba2
junD <yjhut@naver.com>
2026-04-08 15:48:15 +0900

    first_commit

---
+++ build_db.py
@@ -0,0 +1,369 @@
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain.schema import Document
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from transformers import CLIPProcessor, CLIPModel
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import base64 # 이미지 바이트 <-> 문자열
+import fitz 
+import io
+import pandas as pd 
+import numpy as np
+import re
+import gc
+import random
+import os
+os.environ["TORCH_COMPILE_DISABLE"] = "1"
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+from tqdm import tqdm
+from collections import defaultdict
+
+from PIL import Image
+
+from sentence_transformers import SentenceTransformer
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    random.seed(seed)
+    np.random.seed(seed)
+
+import torch._dynamo
+torch._dynamo.config.suppress_errors = True
+
+set_seed(43)
+
+kor_embedding_model_ckpt = 'jhgan/ko-sroberta-multitask'
+clip_ckpt ="Bingsu/clip-vit-base-patch32-ko"
+
+
+############################################################################################
+
+
+langchain_embeddings = HuggingFaceEmbeddings(model_name= kor_embedding_model_ckpt,
+                                             model_kwargs={'device':'cuda:0' },
+                                             encode_kwargs = {'normalize_embeddings': True})
+
+clip_model = CLIPModel.from_pretrained(clip_ckpt, torch_dtype=torch.float16).to(device)
+clip_processor = CLIPProcessor.from_pretrained(clip_ckpt)
+
+
+# path = pdf_file_path
+pdf = fitz.open(path)
+
+
+############################################################################################
+
+# 출처로 활용하기 위해 목차(1.xxx or 2.xxx ...) 추출 코드
+toc_page = pdf[1]
+blocks = toc_page.get_text('blocks')
+blocks.sort(key = lambda b: b[1])
+
+temp_section_dict = {}
+current_question = None
+
+special_char = r'[·]'
+
+for b in blocks:
+    text = b[4].strip()
+    text = re.sub(special_char, " ", text)
+
+    full_match = re.search(r'(Q)(\d+\..+?)\s+(\d+)$', text) 
+    only_question = re.search(r'(Q)(\d+\..+?)$', text)
+    only_page = re.search(r'(\s+)(\d+)$', text)
+
+    if full_match:
+        q = full_match.group(2).strip()
+        p_num = int(full_match.group(3))
+        temp_section_dict.setdefault(p_num, []).append(q)
+        current_question = None
+
+    elif only_question:
+        q = only_question.group(2).strip()
+        current_question = q
+
+    elif only_page:
+        p_num = int(only_page.group(2))
+        temp_section_dict.setdefault(p_num, []).append(current_question)
+        current_question = None
+
+latest_question = []
+page_section_dict = {}
+
+for page_num in range(3, len(pdf) + 1):
+    if page_num in temp_section_dict:
+        latest_question = temp_section_dict[page_num]
+
+    page_section_dict[page_num] = latest_question
+
+
+############################################################################################
+
+
+# 1. 추출 단계에서 페이지 번호를 같이 저장
+def extract_text_with_global_context(pdf_path: str):
+    doc = fitz.open(pdf_path)
+    content = []
+    
+    for page_num in range(len(doc)):
+        if page_num >= 2: 
+            page = doc.load_page(page_num)
+            blocks = page.get_text('dict')['blocks']
+            
+            blocks.sort(key=lambda b: b['bbox'][1])
+            
+            for block in blocks:
+                lines = block.get('lines', [])
+                for line in lines:
+                    line_parts = []
+                    sizes = []
+                    
+                    for span in line['spans']:
+                        text = span['text']
+                        size = span['size']
+                        color = span['color']
+                        
+                        # 파일 내부에서 텍스트 컬러 다른 경우, 강조의 의미로 태깅(**)  처리
+                        processed_text = f"** {text.strip()} ** " if color != 0 else text
+                        line_parts.append(processed_text)
+                        sizes.append(size)
+                    
+                    full_line_text = "".join(line_parts).strip()
+                    if not full_line_text: continue
+                    
+                    max_size = max(sizes) if sizes else 0
+                    
+                    content.append((
+                        full_line_text,
+                        max_size,
+                        page_num + 1,
+                    ))
+                 
+                    
+    return content
+
+
+contents = extract_text_with_global_context(path)
+
+
+def make_chunk_per_page(contents, section_font_size):
+    chunks = []
+    current_chunk_text = []
+    current_page = None
+    # 현재 청크가 새로운 섹션으로 시작하는지 여부
+    current_chunk_is_header = False 
+    
+    tolerance = 0.1
+    
+    for text, size, p_num in contents:
+        clean_text = re.sub(r'-\s*\d+\s*-', '', text).strip()
+        if not clean_text: continue
+
+        is_new_section = abs(size - section_font_size) < tolerance
+        is_new_page = (current_page is not None and p_num != current_page)
+
+        if is_new_section or is_new_page:
+            if current_chunk_text:
+                chunks.append({
+                    "content": "\n".join(current_chunk_text),
+                    "page": current_page,
+                    "is_header": current_chunk_is_header # 이전 청크의 헤더 여부 저장
+                })
+            
+            current_chunk_text = [clean_text]
+            current_page = p_num
+            current_chunk_is_header = True if is_new_section else False
+        else:
+            if current_page is None:
+                current_page = p_num
+                # 첫 시작이 섹션인 경우 처리
+                current_chunk_is_header = True if is_new_section else False
+            current_chunk_text.append(clean_text)
+
+    # 남은 데이터 처리
+    if current_chunk_text:
+        chunks.append({
+            "content": "\n".join(current_chunk_text), 
+            "page": current_page,
+            "is_header": current_chunk_is_header
+        })
+    
+    return chunks
+
+section_chunks = make_chunk_per_page(contents, 15.96) # 15.96 -> 각 섹션의 폰트 size
+
+
+final_documents = []
+current_section_title = "Unknown Section"
+
+for item in section_chunks:
+    p = item['page']
+    content = item['content']
+
+    content = content.replace('∎', '-')
+    
+    # is_header=True일 때만 섹션명 업데이트
+    if item.get('is_header'):
+        current_section_title = content.split('\n')[0][:60]
+
+    final_documents.append(Document(
+        page_content = re.sub('\n', ' ', content),
+        metadata={
+            "page": p,
+            "section": current_section_title, 
+        }
+    ))
+
+# 구분자를 반복적으로 순회하며 청크를 분리하는 재귀분리기 사용
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,  # char 기준
+    chunk_overlap=100,   
+    separators=[". ", "? ", "! ", " ", ""]
+)
+
+    
+split_docs = text_splitter.split_documents(final_documents)
+
+
+############################################################################################
+
+
+vector_db = Chroma(
+    # collection_name= "text_collection", 기본값 = 'langchain'
+    embedding_function = langchain_embeddings,
+    persist_directory = './VectorDB_300_100'
+)
+
+batch_size = 4
+idx = 0
+for i in tqdm(range(0, len(split_docs), batch_size), desc = 'Adding to Chroma'):
+    batch = split_docs[idx : idx + batch_size]
+    vector_db.add_documents(documents = batch)
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    idx += batch_size
+
+
+############################################################################################
+
+
+def extract_images():
+    image_data_list = []
+
+    for page_num in range(len(pdf)):
+        if page_num >=2:
+            current_page = page_num+1
+            page = pdf.load_page(page_num)
+            image_list = page.get_images(full=True)
+
+            sections = page_section_dict.get(current_page, ["unkhown section"])
+
+            joined_sections = " | ".join(sections)
+
+            for image in image_list:
+                xref = image[0]
+                # width = image[2]
+                # height = image[3]
+                bpc = image[4]
+                filter_name = image[8]
+ 
+                if bpc == 1 or filter_name == "FlateDecode":
+                    continue
+
+                base_image = pdf.extract_image(xref)
+                image_bytes = base_image['image']
+
+                image_data_list.append({
+                    'image_bytes': image_bytes,
+                     'page' : current_page,
+                     'section_title': joined_sections
+                })
+                
+    return image_data_list
+
+image_docs = extract_images()
+
+
+def image_to_vectorDB(image_documents :list):
+    embeddings = []
+    metadatas = []
+
+    for i, item in enumerate(image_documents):
+        raw_bytes = item['image_bytes']
+        page_num = item['page']
+        section = item['section_title']
+
+        image = Image.open(io.BytesIO(raw_bytes))
+        inputs = clip_processor(images= image, return_tensors='pt').to(device)
+        with torch.no_grad():
+            img_features = clip_model.get_image_features(**inputs)
+            img_features = img_features / img_features.norm(p=2, dim=-1, keepdim=True) # L2_norm (p=2)
+
+        byte_to_str = base64.b64encode(raw_bytes).decode('utf-8')
+
+        meta = {
+            'page': page_num,
+            'section_title': section,
+            'image_to_str': byte_to_str
+        }
+
+        embeddings.append(img_features.squeeze(0).cpu().numpy().tolist())
+        metadatas.append(meta)
+
+    return embeddings, metadatas
+
+embed, metas = image_to_vectorDB(image_docs)
+
+image_db = Chroma(
+    collection_name='image_collection',
+    persist_directory="./VectorDB_300_100",
+    embedding_function=None
+)
+
+batch_size = 4
+total_images = len(embed)
+
+for i in tqdm(range(0, total_images, batch_size), desc = "Adding Images to DB"):
+    batch_embeds = embed[i : i + batch_size]
+    batch_metas = metas[i : i + batch_size]
+    batch_ids = [f"img_{j+1}" for j in range(i, min(i+batch_size, total_images))]
+
+    image_db._collection.add(
+        ids = batch_ids,
+        embeddings = batch_embeds,
+        metadatas = batch_metas,
+        documents = [" "] * len(batch_ids) # documents=None인 경우 오류 발생하여 빈 문자열 추가
+    )
+
+    gc.collect()
+    torch.cuda.empty_cache()
+
+
+
+if __name__ == "__main__":
+    print("\n" + "="*50)
+    print("RAG DataBase 구축 완료!")
+    print("="*50)
+
+    print(f"저장 경로: {os.path.abspath('./VectorDB_300_100')}")
+    print(f"텍스트 청크 수: {len(split_docs)} 개")
+    print(f"이미지 임베딩 수: {total_images} 개")
+    print("-"*50)
+    print("이제 main.py를 실행하여 답변을 출력할 수 있습니다.")
+    print("="*50 + "\n")
---
+++ main.py
@@ -0,0 +1,261 @@
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_community.retrievers import BM25Retriever
+from langchain.schema import Document
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from transformers import CLIPProcessor, CLIPModel
+from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from qwen_vl_utils import process_vision_info
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import base64 # 이미지 바이트 <-> 문자열
+import fitz 
+import io
+import pandas as pd 
+import numpy as np
+import re
+import gc
+import random
+import os
+os.environ["TORCH_COMPILE_DISABLE"] = "1"
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+from tqdm import tqdm
+from collections import defaultdict
+
+from PIL import Image
+
+from sentence_transformers import SentenceTransformer, CrossEncoder
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    random.seed(seed)
+    np.random.seed(seed)
+
+import torch._dynamo
+torch._dynamo.config.suppress_errors = True
+
+set_seed(43)
+
+kor_embedding_model_ckpt = 'jhgan/ko-sroberta-multitask'
+kor_rerank_model_ckpt= 'dragonkue/bge-reranker-v2-m3-ko' # activation_fn 인자 지정 해야함
+vlm_model_ckpt = "Qwen/Qwen2-VL-7B-Instruct"
+clip_ckpt ="Bingsu/clip-vit-base-patch32-ko"
+
+
+############################################
+
+
+langchain_embeddings = HuggingFaceEmbeddings(model_name= kor_embedding_model_ckpt,
+                                             model_kwargs={'device':'cuda:0' },
+                                             encode_kwargs = {'normalize_embeddings': True})
+
+clip_model = CLIPModel.from_pretrained(clip_ckpt, torch_dtype=torch.float16).to(device)
+clip_processor = CLIPProcessor.from_pretrained(clip_ckpt)
+
+rerank_model = CrossEncoder(kor_rerank_model_ckpt, activation_fn=torch.nn.Softmax(dim = 0)).to(device)
+
+VLM_model = Qwen2VLForConditionalGeneration.from_pretrained(vlm_model_ckpt, torch_dtype = torch.float16).to(device)
+VLM_processor = AutoProcessor.from_pretrained(vlm_model_ckpt)
+
+
+############################################
+
+
+text_db = Chroma(
+    collection_name='langchain',
+    persist_directory="VectorDB_300_100/",
+    embedding_function=langchain_embeddings
+)
+
+image_db = Chroma(
+    collection_name='image_collection',
+    persist_directory="VectorDB_300_100/",
+    embedding_function=None
+)
+
+
+############################################
+
+
+data_list = text_db.get() 
+all_chunks = [
+    Document(page_content=text)
+    for text in data_list['documents']
+]  # BM25 사용위해 모든 청크 준비 
+
+bm25 = BM25Retriever.from_documents(all_chunks)
+bm25.k = 10
+
+def re_ranking(query):
+    
+    candidate_contexts = []
+    documents = bm25.get_relevant_documents(query)
+    for i in range(len(documents)):
+        candidate_contexts.append(documents[i].page_content)
+    
+    
+    rank_list = [[query, candidate] for candidate in candidate_contexts]
+    score = rerank_model.predict(rank_list)
+    
+    
+    max_index = np.argmax(score)
+    similar_context = candidate_contexts[max_index]
+    
+    print()
+    print(f"Rerank 결과: {len(candidate_contexts)}개 Document 중, Document_{max_index+1} 선택 | (유사도:{np.round(score[max_index],3)})")
+
+    return similar_context
+
+
+############################################
+
+
+def get_last_document(query: str):
+    best_doc = re_ranking(query)
+
+    results = text_db.get(
+                where_document={"$contains" : best_doc}
+                )
+    
+    use_document= []
+    item = {
+        'context' : results['documents'][0],
+        'metadata': results['metadatas'][0]
+    }
+    use_document.append(item)
+    page = results['metadatas'][0]['page']
+
+    return use_document, page
+
+    
+############################################
+
+
+def extract_best_similar_image_with_query(query: str, page : int):
+
+    inputs = clip_processor(text=[query], return_tensors='pt', padding=True).to(device)
+    with torch.no_grad():
+        query_vec = clip_model.get_text_features(**inputs)
+        query_vec = query_vec / query_vec.norm(p=2, dim=-1, keepdim=True)
+        query_vec = query_vec.squeeze(0).cpu().numpy().tolist()
+    
+    best_query_similar_image = image_db.similarity_search_by_vector(
+        embedding=query_vec,
+        k = 1,                # 가장 유사한 이미지 1개만 추출
+        filter={"page": page} # 해당 페이지의 이미지를 후보로
+    )
+     
+    # 해당 페이지에 이미지 없는 경우
+    if not best_query_similar_image:
+        print("해당 페이지에 이미지 없음!")
+        return None
+    
+    image_bytes = base64.b64decode(best_query_similar_image[0].metadata['image_to_str'])
+    extract_image = Image.open(io.BytesIO(image_bytes))
+
+    return extract_image
+
+
+############################################
+
+
+def get_answer(query, document, image):
+    
+    content = document[0]['context']
+    page = document[0]['metadata']['page']
+    section = document[0]['metadata']['section']
+    
+    messages = [
+    {
+        "role": "system", 
+        "content": "~~~" # 모델에 진행할 task 부여
+    },
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "text", 
+                "text": f"""
+                    ### [추가 정보]
+                    {content} 
+                    
+                    ### [질문]
+                    {query}
+                    
+                    ### [답변]              
+                    -  답변: (질문에 대한 핵심 결론을 15자 내외의 한 문장으로 간결하게 작성)
+                    -  이미지 설명: (이미지 의미 있는 정보라면 핵심 내용을 간략히 요약해서 설명. 만약 질문과 관련 없는 단순 배경/아이콘이라면 '해당 없음'으로 표기)
+                    -  추가설명: (질문과 관련된 유의사항, 예외 조건, 또는 구체적인 포맷 등 중요한 정보를 작성. 불필요한 경우 '해당 없음'으로 표기. 필요시 첨부된 이미지를 참고하여 작성)
+                    """
+            },
+            {
+                "type": "image", 
+                "image": image
+            }
+                    ]
+    }
+        
+                ]
+    
+
+    text = VLM_processor.apply_chat_template(messages, tokenize = False,
+                                             add_generation_prompt = True)
+    
+    image_inputs, _ = process_vision_info(messages)
+    inputs = VLM_processor(text=[text],
+                           images=image_inputs,
+                           padding=True, 
+                           return_tensors = 'pt').to(device)
+    
+    
+    generate_ids = VLM_model.generate(**inputs, 
+                                      max_new_tokens=300, 
+                                      repetition_penalty=1.2,  # 중복 억제 파라미터
+                                      do_sample=False # 가장 높은 확률의 단어만 사용하는 greedy search        
+                                     )
+
+    
+    res = VLM_processor.decode(generate_ids[0][inputs.input_ids.shape[1]:], skip_special_tokens=True)
+
+    final_answer = f"{res}\n- 관련 목차: {section}\n- 근거: {page}p"
+
+    return final_answer
+
+
+if __name__ == "__main__":
+    print("\n" + "="*50)
+    print("민원 RAG 시스템이 준비되었습니다.")
+    print("="*50)
+
+    while True:
+        user_query = input("\n Q. 질문을 입력하세요!(종료하려면 q를 누르세요)")
+
+        if user_query.lower() == 'q':
+            print("시스템을 종료합니다.")
+            break
+            
+        print("\n[답변 생성 중....]")
+
+        last_document, target_page = get_last_document(user_query)
+        image = extract_best_similar_image_with_query(user_query, target_page)
+        answer = get_answer(user_query, last_document, image)
+
+        print("\n" + "="*50)
+        print("[답변]")
+        print(answer)
+        print("-"*50)
